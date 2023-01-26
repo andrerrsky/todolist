5b167cd1188a3a80b5d4143d35ec95c0a689a0c5
Andre Fortunato <andre.fortunato@mercadolivre.com>
2023-01-26 13:19:22 -0300

    chore: files adjustments

--- src/services/baseConfig.ts
+++ src/services/baseConfig.ts
@@ -1,12 +1,11 @@
 import * as axios from "axios";
 import { config } from "../config";
-import { AUTH_TOKEN_KEY } from "../hooks/useAuth/useAuth.hook";
 
 export const request = () => {
   return axios.default.create({
     baseURL: config.apiURL,
     headers: {
-      Authorization: localStorage.getItem(AUTH_TOKEN_KEY),
+      Authorization: "token",
     },
   });
 };
